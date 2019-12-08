8a8340ff836ac54ffc8408c66ed1e299ab784b42
Bijal Patel <bijal.patel4@gmail.com>
2019-12-07 16:34:44 -0800

    Updates

---
+++ precache-manifest.3c44cdc539652959eab645725295d334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2217515ff8fa4f9451d6a6962306fb05",
+    "url": "/timesheet-app/index.html"
+  },
+  {
+    "revision": "47f503b2c76029fb1c4d",
+    "url": "/timesheet-app/static/css/2.1c29a7ee.chunk.css"
+  },
+  {
+    "revision": "b59fe98a16d86fcb80df",
+    "url": "/timesheet-app/static/css/main.69d916e6.chunk.css"
+  },
+  {
+    "revision": "47f503b2c76029fb1c4d",
+    "url": "/timesheet-app/static/js/2.afb75d43.chunk.js"
+  },
+  {
+    "revision": "b59fe98a16d86fcb80df",
+    "url": "/timesheet-app/static/js/main.d8c3cc7c.chunk.js"
+  },
+  {
+    "revision": "498494554a3324e45706",
+    "url": "/timesheet-app/static/js/runtime-main.b8db17fc.js"
+  },
+  {
+    "revision": "8c822da7ba505ef2cb362a796a68a3b6",
+    "url": "/timesheet-app/static/media/logo.8c822da7.png"
+  }
+]);
